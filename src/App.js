f2cd30776a0a66fecf635642a878f5055f41bb5a
Shivam Kumar <manorinfinity@Shivams-MacBook-Air.local>
2024-01-06 13:41:20 +0530

    added horizontal smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import './App.css';
-import MainPage from './components/MainPage';
-import SecondPage from './components/SecondPage';
-import About from './components/About';
-
-
-
-
+import "./App.css";
 
 function App() {
   const handleScroll = (event) => {
-    const container = event.target;
-    const scrollAmount = event.deltaY;
-    container.scrollTo({
-      top: 0,
-      left: container.scrollLeft + scrollAmount,
-      behavior: 'smooth'
-    });
+    const direction = event.deltaY;
+    console.log(direction);
+    if (direction >= 0) {
+      console.log(window.pageXOffset + window.innerWidth);
+      window.scrollTo({
+        left: window.pageXOffset + window.innerWidth,
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo({
+        left: window.pageXOffset - window.innerWidth,
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
     <div>
-      <header>
-        {/* Header content */}
-      </header>
-      <div className="container" onWheel={handleScroll}>
-        <div className="page">
-          <MainPage/>
-        </div>
-        <div className="page">
-          <SecondPage/>
-        </div>
-        <div className="page">
-         <About/>
-        </div>
+      <header>{/* Header content */}</header>
+      <div className="main-container" onWheel={handleScroll}>
+        <div className="content-container"></div>
+        <div className="content-container"></div>
+        <div className="content-container"></div>
+        <div className="content-container"></div>
       </div>
-      <footer>
-        {/* Footer content */}
-      </footer>
+      <footer>{/* Footer content */}</footer>
     </div>
   );
 }
